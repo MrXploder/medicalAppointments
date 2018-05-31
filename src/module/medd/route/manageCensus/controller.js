bcda19dbfab7fd0cb2466e3e96d17b940315210e
MrXploder <l.arancibiaf@gmail.com>
2018-05-31 03:17:38 -0400

    update

--- src/module/medd/route/manageCensus/controller.js
+++ src/module/medd/route/manageCensus/controller.js
@@ -1,22 +1,21 @@
 (function(){
-   angular
-   .module('angularApp')
-   .controller('manageCensusController', manageCensusController);
+	angular
+	.module('angularApp')
+	.controller('manageCensusController', manageCensusController);
 
-   manageCensusController.$inject = ["Appointments", "Doctors", "Patients", "Operators", "$scope", "$timeout", "$location", "$localStorage"];
+	manageCensusController.$inject = ["Appointments", "Doctors", "Patients", "Operators", "$scope", "$timeout", "$location", "$localStorage"];
 
-   function manageCensusController(Appointments, Doctors, Patients, Operators, $scope, $timeout, $location, $localStorage){
-   	var mcc = this;
+	function manageCensusController(Appointments, Doctors, Patients, Operators, $scope, $timeout, $location, $localStorage){
+		var mcc = this;
 
-   	mcc.patientFilter = '-1';
-   	mcc.doctorFilter = '-1';
-   	mcc.appointments = Appointments.query();
-   	mcc.doctors = Doctors.query();
-   	mcc.patients = Patients.query();
-   	mcc.changeStatus = changeStatus;
+		mcc.filters = {byPatient: '-1', byDoctor: '-1', searchTerm: null, sinceDate: null, tillDate: null};
+		mcc.appointments = Appointments.query();
+		mcc.doctors = Doctors.query();
+		mcc.patients = Patients.query();
+		mcc.changeStatus = changeStatus;
 
-   	function changeStatus(){
+		function changeStatus(){
 
-   	}
-   }
+		}
+	}
 })();
