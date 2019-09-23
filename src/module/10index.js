b137f07e83ac340657cb93dde1ef4676eafd7270
Luis <l.arancibiaf@gmail.com>
2019-09-23 02:39:18 -0300

    clean up before merge

--- src/module/10index.js
+++ src/module/10index.js
@@ -1,9 +1,9 @@
 (function(angular, undefined) {
 'use strict';
 
-angular.module('angularApp', ['ngRoute', 'ngStorage', 'ngResource', 'ngDialog', 'ngRut', 'ngLoadingBar', 'ngDirPagination', 'ngMaterialize', 'ngTemplates', 'ui.bootstrap.contextMenu'])
+angular.module('angularApp', ['ngRoute', 'ngStorage', 'ngResource', 'ngDialog', 'cp.ngConfirm', 'platanus.rut', 'angular-loading-bar', 'angularUtils.directives.dirPagination', 'ui.materialize', 'indexedDB', 'templates-main'])
 
-.constant('ENV', {SHA:'8bf468d2f64d3efab5689033d411086d2787bdae',name:'development',shortSHA:'8bf468d',lastCommitTime:'"2018-09-24 03:43:39 -0300"',currentUser:'MrXploder',lastCommitMessage:'"update\r\n\r\nAgregado el calendario al menu "home"\r\nMejoradas las plantillas de impresion\r\nAgregado el click secundario en el calendario\r\n"',lastCommitNumber:'51',lastCommitAuthor:'"MrXploder"'})
+.constant('ENV', {lastCommitMessage:'"do not delete all src files\r\n\r\n"',lastCommitTime:'"2019-09-23 02:31:24 -0300"',currentUser:'Luis',shortSHA:'414f9d4',SHA:'414f9d46845234218446bdbe7956dff7ad96299d',name:'development',lastCommitAuthor:'"Luis"',lastCommitNumber:'56'})
 
 ;
 })(angular);
